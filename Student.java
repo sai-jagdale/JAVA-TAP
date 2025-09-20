621274daea820350803ebfef2969336e61fa9521
sai.jagdale <161121425+sai-jagdale@users.noreply.github.com>
2025-09-20 17:00:33 +0530

    taken input using Util.Scanner class

--- Student.java
+++ Student.java
@@ -1,7 +1,9 @@
+import java.util.*;
+
 class Student {
 
-    String Name;
-    int RollNo;
+    private String Name;
+    private int RollNo;
     float Marks;
     int Standard;
 
@@ -34,6 +36,21 @@ class Student {
         return ("Name : "+Name+" "+"RollNo : "+RollNo+" "+"Marks : "+Marks+" "+"Standard : "+Standard );
     }
 
+    // Getter Setter for Name
+    public void setName(String Name){
+        this.Name=Name;
+    }
+    public String getName(String Name){
+        return Name;
+    }
+    // Getter Setter for RollNo
+    public void setRollNo(int RollNo){
+        this.RollNo=RollNo;
+    }
+    public int getRollNo(int RollNo){
+        return RollNo;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
@@ -49,6 +66,24 @@ class Student {
         Student s3 = new Student(s2);
         System.out.println(s3);
 
+        System.out.println("---------------------Taking Input --------------------------------------------");
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter Name : ");
+        String SName=input.nextLine();
+
+        System.out.println("Enter RollNO : ");
+        int SRollNo=input.nextInt();
+
+        System.out.println("Enter Marks : ");
+        float SMarks=input.nextFloat();
+
+        System.out.println("Enter Standard : ");
+        int SStandard=input.nextInt();
+
+        Student s4 = new Student( SName , SRollNo ,SMarks,SStandard );
+        System.out.println(s4);
 
     }
 }
